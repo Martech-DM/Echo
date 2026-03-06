b3d50af381982361937cc4ef8f5919d3e41e7d18
realcodesiman <realcodesiman@gmail.com>
2026-03-06 11:42:36 +0700

    feat: add orpc openapi (#272)
    * feat: add orpc openapi
    
    * feat: add public tags API
    
    * feat: add public API for custom-fields
    
    * feat: add flows list API

--- packages/database/src/drizzle/schema.ts
+++ packages/database/src/drizzle/schema.ts
@@ -16,6 +16,8 @@ import {
   vector,
 } from "drizzle-orm/pg-core"
 
+export * from "drizzle-orm/zod"
+
 export const logType = pgEnum("LogType", ["error", "audit"])
 export const customFieldType = pgEnum("CustomFieldType", [
   "shortText",
@@ -61,15 +63,6 @@ export const broadcastSchedulesType = pgEnum("BroadcastSchedulesType", [
 export const inboxType = pgEnum("InboxType", [
   "webchat",
   "messenger",
-  "whatsapp",
-  "zalo",
-])
-export const integrationType = pgEnum("IntegrationType", [
-  "webchat",
-  "googleSheets",
-  "messenger",
-  "openai",
-  "gemini",
   "whatsapp",
   "zalo",
 ])
@@ -527,6 +520,7 @@ export const chatbotModel = pgTable("Chatbot", {
       name: "Chatbot_organizationId_fkey",
     }),
   plan: text().default("free").notNull(),
+  token: text(),
 })
 
 export const chatbotMemberModel = pgTable("ChatbotMember", {
@@ -743,7 +737,7 @@ export const conversationModel = pgTable(
     archivedAt: timestamp({ precision: 3 }),
     inboxType: inboxType().notNull().default("webchat"),
     sourceId: text(),
-    conversationAttributes: jsonb(),
+    conversationAttributes: jsonb().$type<{ [x: string]: unknown }>(),
     contactLastSeenAt: timestamp({ precision: 3 }),
     agentLastSeenAt: timestamp({ precision: 3 }),
     lastActivityAt: timestamp({ precision: 3 }).defaultNow().notNull(),
@@ -963,8 +957,10 @@ export const flowVersionModel = pgTable("FlowVersion", {
       onUpdate: "cascade",
       name: "FlowVersion_flowId_fkey",
     }),
-  nodes: jsonb().notNull(),
-  edges: jsonb().notNull(),
+  // biome-ignore lint/suspicious/noExplicitAny: use any
+  nodes: jsonb().$type<any[]>().notNull(),
+  // biome-ignore lint/suspicious/noExplicitAny: use any
+  edges: jsonb().$type<any[]>().notNull(),
   isDraft: boolean().notNull(),
   isLatest: boolean().default(false).notNull(),
   startNodeId: text().notNull(),
@@ -1108,12 +1104,17 @@ export const integrationModel = pgTable(
         onUpdate: "cascade",
         name: "Integration_chatbotId_fkey",
       }),
-    integrationType: integrationType().notNull(),
+    integrationType: text().notNull(),
   },
   (table) => [
     index("Integration_chatbotId_idx").using(
       "btree",
       table.chatbotId.asc().nullsLast().op("text_ops"),
+    ),
+    index("Integration_chatbotId_integrationType_key").using(
+      "btree",
+      table.chatbotId.asc().nullsLast().op("text_ops"),
+      table.integrationType.asc().nullsLast().op("text_ops"),
     ),
   ],
 )
@@ -1560,7 +1561,7 @@ export const messageModel = pgTable(
         name: "Message_chatbotId_fkey",
       }),
     content: text(),
-    contentAttributes: jsonb(),
+    contentAttributes: jsonb().$type<{ [x: string]: unknown }>(),
     messageType: messageType().notNull(),
     contentType: contentType().notNull(),
     senderType: senderType().notNull(),
@@ -1824,4 +1825,15 @@ export const whatsappMessageTemplateModel = pgTable("WhatsappMessageTemplate", {
   language: text().notNull(),
   category: text().notNull(),
   status: text().notNull(),
+})
+
+export const jwkModel = pgTable("jwks", {
+  id: text().primaryKey(),
+  publicKey: text().notNull(),
+  privateKey: text().notNull(),
+  createdAt: timestamp({
+    precision: 6,
+    withTimezone: true,
+  }).notNull(),
+  expiresAt: timestamp({ precision: 6, withTimezone: true }),
 })
